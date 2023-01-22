c8905932bb27a8eb3e454673c54294c1a9b7b19d
Anish <aniish9.18@gmail.com>
2023-01-22 20:26:49 +0530

    First commit

--- Acuity/settings.py
+++ Acuity/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-3n527n*v!0at23hx=z5-dl)3(cb85wj5shh1k@zzn8!%j)#9vr"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
