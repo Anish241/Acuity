7a6c2aeb871c880bd6d00385bf4100040d2bfebb
Anish <aniish9.18@gmail.com>
2024-04-17 17:03:08 +0530

    settings

--- Acuity/settings.py
+++ Acuity/settings.py
@@ -21,13 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRET"]
+SECRET_KEY = "django-insecure-3n527n*v!0at23hx=z5-dl)3(cb85wj5shh1k@zzn8!%j)#9vr"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [os.environ["WEBSITE_HOSTNAME"]]
-CSRF_TRUSTED_ORIGINS = ["https://" + os.environ["WEBSITE_HOSTNAME"]]
+ALLOWED_HOSTS = ["*"]
+
 
 
 # Application definition
@@ -80,19 +80,15 @@ WSGI_APPLICATION = "Acuity.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-connection_string = os.environ["AZURE_POSTGRESQL_CONNECTIONSTRING"]
-paramaters = {pair.split("="):pair.split("=")[1] for pair in connection_string.split(" ")}
 
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": paramaters["dbname"],
-        "HOST": paramaters["host"],
-        "USER": paramaters["user"],
-        "PASSWORD": paramaters["password"],
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
 
 
 # Password validation
